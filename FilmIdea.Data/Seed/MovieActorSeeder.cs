ce899e595a818824278aadf68a1f58632d467ef1
Nasko Stanoev <nasko.stanoevv@gmail.com>
2023-08-12 00:16:42 +0300

    seed movieActors and movieGenres

--- FilmIdea.Data/Seed/MovieActorSeeder.cs
+++ FilmIdea.Data/Seed/MovieActorSeeder.cs
@@ -17,7 +17,127 @@ public class MovieActorSeeder
             {
                 ActorId = 1,
                 MovieId = 1
-            }
+            },
+            new MovieActor()
+            {
+                ActorId = 1003,
+                MovieId = 2
+            },
+            new MovieActor()
+            {
+                ActorId = 1020,
+                MovieId =2
+            },
+            new MovieActor()
+            {
+                ActorId = 1007,
+                MovieId =1001
+            },
+            new MovieActor()
+            {
+                ActorId = 1016,
+                MovieId =1001
+            },
+            new MovieActor()
+            {
+                ActorId = 1017,
+                MovieId =1001
+            },
+            new MovieActor()
+            {
+                ActorId = 1007,
+                MovieId =1007
+            },
+            new MovieActor()
+            {
+                ActorId = 1014,
+                MovieId =1007
+            },
+            new MovieActor()
+            {
+                ActorId = 1016,
+                MovieId =1007
+            },
+            new MovieActor()
+            {
+                ActorId = 1003,
+                MovieId =1009
+            },
+            new MovieActor()
+            {
+                ActorId = 1013,
+                MovieId =1009
+            },
+            new MovieActor()
+            {
+                ActorId = 1015,
+                MovieId =1009
+            },
+            new MovieActor()
+            {
+                ActorId = 1020,
+                MovieId =1012
+            },
+            new MovieActor()
+            {
+                ActorId = 1013,
+                MovieId =1013
+            },
+            new MovieActor()
+            {
+                ActorId = 1006,
+                MovieId =1014
+            },
+            new MovieActor()
+            {
+                ActorId = 1007,
+                MovieId =1014
+            },
+            new MovieActor()
+            {
+                ActorId = 1011,
+                MovieId =1014
+            },
+            new MovieActor()
+            {
+                ActorId = 1016,
+                MovieId =1014
+            },
+            new MovieActor()
+            {
+                ActorId = 1007,
+                MovieId =1015
+            },
+            new MovieActor()
+            {
+                ActorId = 1008,
+                MovieId =1015
+            },
+            new MovieActor()
+            {
+                ActorId = 1010,
+                MovieId =1016
+            },
+            new MovieActor()
+            {
+                ActorId = 1011,
+                MovieId =1016
+            },
+            new MovieActor()
+            {
+                ActorId = 1014,
+                MovieId =1017
+            },
+            new MovieActor()
+            {
+                ActorId = 1009,
+                MovieId =1018
+            },
+            new MovieActor()
+            {
+                ActorId = 1010,
+                MovieId =1019
+            },
         };
     }
 }
--- FilmIdea.Data/Seed/MovieGenreSeeder.cs
+++ FilmIdea.Data/Seed/MovieGenreSeeder.cs
@@ -17,7 +17,197 @@ public class MovieGenreSeeder
             {
                 GenreId = 2,
                 MovieId = 2
-            }
+            },
+            new MovieGenre()
+            {
+                GenreId = 1006,
+                MovieId = 1
+            },
+            new MovieGenre()
+            {
+                GenreId = 1007,
+                MovieId =1
+            },
+            new MovieGenre()
+            {
+                GenreId = 1010,
+                MovieId =1
+            },
+            new MovieGenre()
+            {
+                GenreId = 1014,
+                MovieId =1
+            },
+            new MovieGenre()
+            {
+                GenreId = 2,
+                MovieId =1
+            },
+            new MovieGenre()
+            {
+                GenreId = 1007,
+                MovieId =2
+            },
+            new MovieGenre()
+            {
+                GenreId = 1017,
+                MovieId =2
+            },
+            new MovieGenre()
+            {
+                GenreId = 1012,
+                MovieId =4
+            },
+            new MovieGenre()
+            {
+                GenreId = 1013,
+                MovieId =4
+            },
+            new MovieGenre()
+            {
+                GenreId = 1014,
+                MovieId =4
+            },
+            new MovieGenre()
+            {
+                GenreId = 1003,
+                MovieId =5
+            },
+            new MovieGenre()
+            {
+                GenreId = 1009,
+                MovieId =5
+            },
+            new MovieGenre()
+            {
+                GenreId = 1013,
+                MovieId =5
+            },
+            new MovieGenre()
+            {
+                GenreId = 1015,
+                MovieId =5
+            },
+            new MovieGenre()
+            {
+                GenreId = 1012,
+                MovieId =7
+            },
+            new MovieGenre()
+            {
+                GenreId = 1013,
+                MovieId =7
+            },
+            new MovieGenre()
+            {
+                GenreId = 1010,
+                MovieId =10
+            },
+            new MovieGenre()
+            {
+                GenreId = 1017,
+                MovieId =10
+            },
+            new MovieGenre()
+            {
+                GenreId = 1006,
+                MovieId =11
+            },
+            new MovieGenre()
+            {
+                GenreId = 1007,
+                MovieId =11
+            },
+            new MovieGenre()
+            {
+                GenreId = 1008,
+                MovieId =11
+            },
+            new MovieGenre()
+            {
+                GenreId = 1010,
+                MovieId =11
+            },
+            new MovieGenre()
+            {
+                GenreId = 1012,
+                MovieId =11
+            },
+            new MovieGenre()
+            {
+                GenreId = 1015,
+                MovieId =11
+            },
+            new MovieGenre()
+            {
+                GenreId = 1016,
+                MovieId =11
+            },
+            new MovieGenre()
+            {
+                GenreId = 1017,
+                MovieId =11
+            },
+            new MovieGenre()
+            {
+                GenreId = 1020,
+                MovieId =11
+            },
+            new MovieGenre()
+            {
+                GenreId = 1003,
+                MovieId =14
+            },
+            new MovieGenre()
+            {
+                GenreId = 1011,
+                MovieId =15
+            },
+            new MovieGenre()
+            {
+                GenreId = 1014,
+                MovieId =15
+            },
+            new MovieGenre()
+            {
+                GenreId = 1003,
+                MovieId =16
+            },
+            new MovieGenre()
+            {
+                GenreId = 1006,
+                MovieId =17
+            },
+            new MovieGenre()
+            {
+                GenreId = 1011,
+                MovieId =17
+            },
+            new MovieGenre()
+            {
+                GenreId = 1007,
+                MovieId =19
+            },
+            new MovieGenre()
+            {
+                GenreId = 1008,
+                MovieId =19
+            },
+            new MovieGenre()
+            {
+                GenreId = 1011,
+                MovieId =19
+            },
+            new MovieGenre()
+            {
+                GenreId = 1015,
+                MovieId =19
+            },
+            new MovieGenre()
+            {
+                GenreId = 1016,
+                MovieId =19
+            },
         };
     }
 }
